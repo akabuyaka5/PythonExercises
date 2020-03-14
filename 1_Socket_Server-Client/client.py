263a6a5a75191e3bb77c6f806fc837a04ea0b0e9
agala <agala@datiobd.com>
2020-03-14 19:45:44 +0100

    fixed encoding

--- 1_Socket_Server-Client/client.py
+++ 1_Socket_Server-Client/client.py
@@ -5,12 +5,12 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('127.0.0.1', 52186)
-print('connecting to %s port %s', server_address)
+print('Connecting to %s port %s', server_address)
 sock.connect(server_address)
 
 try:
-    message = b'This is the message.  It will be repeated.'
-    print(b'sending "%s"', message)
+    message = 'This is the message. It will be repeated.'.encode()
+    print('SENDING:', message.decode())
     sock.sendall(message)
 
     amount_received = 0
@@ -19,7 +19,7 @@ try:
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print('received "%s"', data)
+        print('RECEIVED:', data.decode())
 
 finally:
     print('closing socket')
--- 1_Socket_Server-Client/server.py
+++ 1_Socket_Server-Client/server.py
@@ -5,25 +5,25 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('127.0.0.1', 52186)
-print('starting up on %s port %s', server_address)
+print('Starting up on port %s', server_address)
 sock.bind(server_address)
 
 sock.listen()
 
 while True:
-    print('waiting for a connection')
+    print('Waiting for a connection')
     connection, client_address = sock.accept()
     try:
-        print('connection from', client_address)
+        print('Connection from', client_address)
 
         while True:
             data = connection.recv(16)
-            print('received "%s"', data)
+            print('RECEIVED:', data.decode())
             if data:
-                print('sending data back to the client')
+                print('Sending data back to the client')
                 connection.sendall(data)
             else:
-                print('no more data from', client_address)
+                print('No more data from', client_address)
                 break
 
     finally:
