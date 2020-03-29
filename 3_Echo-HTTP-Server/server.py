b44d56e39f394b2cffc66ee8f72722946efbcf4a
Adrian Perez Fernandez <b.garcia@dvibor.com>
2020-03-29 18:23:15 +0200

    Add changes to Practica 3

--- 3_Echo-HTTP-Server/server.py
+++ 3_Echo-HTTP-Server/server.py
@@ -2,8 +2,8 @@
 
 import socket
 from Requests import requests
-im
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+string = "HTTP/1.1 200 OK\r\nCache-Control: no-store\r\nContent-Length: 15\r\nContent-Type: text/plain; charset=utf-8\r\nConnection: close\r\n\r\nHello World!!!!"
 
 server_address = ('127.0.0.1', 5005)
 print('Starting up on port %s', server_address)
@@ -21,7 +21,7 @@ while not sendall:
         request = requests(data.decode())
         if data:
             print('Sending data back to the client')
-            connection.sendall(data)
+            connection.sendall(string.encode())
             sendall = True
         else:
             print('No more data from', client_address)
